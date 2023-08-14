62c72b800e08ab53e2b0e39e25839b24116f6168
Fahari Hamada Sidi <mkl.devops@gmail.com>
2023-08-14 13:24:18 +0200

    feat: add .template dir

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,11 @@
 
 FROM mkldevops/php-fpm-alpine:8.2
 
+COPY --link docker/docker-entrypoint.sh /usr/local/bin/docker-entrypoint
+RUN chmod +x /usr/local/bin/docker-entrypoint
 COPY --link docker/app.ini /usr/local/etc/php/conf.d/app.ini
+
+USER www-data
 
 EXPOSE 80
 ENTRYPOINT ["docker-entrypoint"]
--- docker/install.sh
+++ docker/install.sh
@@ -21,7 +21,28 @@ rm -Rf tmp/
 ! test -f .env.local && echo 'DATABASE_URL="sqlite:///%kernel.project_dir%/var/data.db"' > .env.local
 symfony console doctrine:schema:create
 
-symfony composer req --dev phpstan/phpstan cs-fixer rector/rector in
-symfony composer req admin api webonyx/graphql-php
+symfony composer req --dev phpstan/phpstan \
+  phpstan/extension-installer \
+  phpstan/phpstan-deprecation-rules  \
+  phpstan/phpstan-phpunit \
+  phpstan/phpstan-strict-rules \
+  phpstan/phpstan-webmozart-assert \
+  phpstan/phpstan-doctrine \
+  phpstan/phpstan-symfony \
+  cs-fixer \
+  rector/rector \
+  infection/infection \
+  hautelook/alice-bundle \
+  phpunit/phpunit \
+  dama/doctrine-test-bundle;
+
+
+symfony composer req admin \
+  api \
+  webonyx/graphql-php \
+  gesdinet/jwt-refresh-token-bundle \
+  lexik/jwt-authentication-bundle \
+  nelmio/cors-bundle \
+  sentry/sentry-symfony;
 
 cp -Rp .template/* .
